aaa0a6f62162ca16da7e9157f3cf7610b1339df5
Heloise-Truong <heloisetruong@orange.fr>
2022-01-25 22:55:02 +0100

    commit firebase

---
+++ app/src/main/java/fr/ubo/master/morisen_hta_alr/MainActivity.java
@@ -0,0 +1,158 @@
+package fr.ubo.master.morisen_hta_alr;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthSettings;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "tag";
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+
+    private ListView listViewPlayers;
+    private ArrayList<String> arrayListPlayers = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lunchAuthentifcation();
+
+        firebaseRef = FirebaseDatabase.getInstance().getReference();
+
+    }
+
+    public void lunchAuthentifcation() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.PhoneBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build();
+        registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+            @Override
+            public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                IdpResponse response = result.getIdpResponse();
+                if (result.getResultCode() == RESULT_OK) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.i(TAG, "USER Ok : " + user.getUid());
+                } else {
+// Erreur
+                }
+            }
+        }).launch(signInIntent);
+
+    }
+
+    public void testPhoneVerify() {
+        // [START auth_test_phone_verify]
+        String phoneNum = "+16505554567";
+        String testVerificationCode = "123456";
+
+        // Whenever verification is triggered with the whitelisted number,
+        // provided it is not set for auto-retrieval, onCodeSent will be triggered.
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNum)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                @Override
+                public void onCodeSent(String verificationId,
+                     PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        // Save the verification id somewhere
+
+                        // The corresponding whitelisted code above should be used to complete sign-in.
+                        MainActivity.this.enableUserManuallyInputCode();
+                    }
+
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                        // Sign in with the credential
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+                    }
+                })
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+        // [END auth_test_phone_verify]
+    }
+
+    public void testPhoneAutoRetrieve() {
+        // [START auth_test_phone_auto]
+        // The test phone number and code should be whitelisted in the console.
+        String phoneNumber = "+16505554567";
+        String smsCode = "123456";
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuthSettings firebaseAuthSettings = firebaseAuth.getFirebaseAuthSettings();
+
+        // Configure faking the auto-retrieval with the whitelisted numbers.
+        ((FirebaseAuthSettings) firebaseAuthSettings).setAutoRetrievedSmsCodeForPhoneNumber(phoneNumber, smsCode);
+
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(firebaseAuth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential credential) {
+                        // Instant verification is applied and a credential is directly returned.
+                        // ...
+                    }
+
+                    // [START_EXCLUDE]
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+
+                    }
+                    // [END_EXCLUDE]
+                })
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+        // [END auth_test_phone_auto]
+    }
+
+    public void signOut() {
+        // [START auth_sign_out]
+        FirebaseAuth.getInstance().signOut();
+        // [END auth_sign_out]
+    }
+
+    private void enableUserManuallyInputCode() {
+        // No-op
+    }
+}
